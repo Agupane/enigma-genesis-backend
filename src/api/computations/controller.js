590d16365324a2ebd56826692511cc342e6d776e
agustin.pane@gmail.com <agustin.pane@gmail.com>
2019-09-20 12:11:48 -0300

    Update package json Adds getComputationsByPage route

--- src/api/computations/controller.js
+++ src/api/computations/controller.js
@@ -22,3 +22,33 @@ export const getAllComputations = async (req, res, next) => {
   }
   res.json(results)
 }
+
+export const getComputationsByPage = async (req, res, next) => {
+  console.log('Geting all computations by page...')
+  const { page, rowsPerPage, order, orderBy } = req.query
+  console.log(`Rows per page: ${rowsPerPage}, page: ${page}, order column: ${order}, orderDirection: ${orderBy}`)
+  const rowsPerPageNum = parseInt(rowsPerPage)
+  let results = []
+  let totalRows = 0
+  try {
+    const sortOrder = order === 'desc' ? -1 : 1
+    const computations = await Computation.find({}).sort([[ orderBy, sortOrder]]).skip(rowsPerPageNum*page).limit(rowsPerPageNum)
+    totalRows = await Computation.count({})
+    for (let computationIterator of computations) {
+      const status = getTaskStatus(computationIterator)
+
+      const newComputation = {
+        ...computationIterator.toObject(),
+        status
+      }
+      results.push(newComputation)
+    }
+  } catch (error) {
+    console.log('Error on getComputationsByPage:', error)
+    next(error)
+  }
+  res.json({
+    totalRows,
+    results
+  })
+}
--- src/api/computations/routes.js
+++ src/api/computations/routes.js
@@ -1,8 +1,9 @@
-import { getAllComputations } from './controller'
+import { getAllComputations, getComputationsByPage } from './controller'
 import express from 'express'
 
 const computationRoutes = express.Router()
 
 computationRoutes.get('/', getAllComputations)
+computationRoutes.get('/:pages/', getComputationsByPage)
 
 export default computationRoutes
